2c5851f14aa9a739fa2444119cbd048ef6fd5c1f
PythonCharro <94500126+PythonCharro@users.noreply.github.com>
2022-01-15 17:21:57 -0800

    Move database to POSTGRE

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ from flask_gravatar import Gravatar
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['APP_KEY']
+app.config['SECRET_KEY'] = os.environ.get("APP_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
